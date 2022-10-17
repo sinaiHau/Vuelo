f2cce3d4c57a33bdfe97933279f164a2c9ff3269
YulisaKau <113855888+YulisaKau@users.noreply.github.com>
2022-10-17 18:55:05 -0500

    agrego mis cambios. Yulisa

--- Vuelo/src/com/tuempresa/vuelo/modelo/Aerolinea.java
+++ Vuelo/src/com/tuempresa/vuelo/modelo/Aerolinea.java
@@ -1,5 +1,16 @@
 package com.tuempresa.vuelo.modelo;
 
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
 public class Aerolinea {
+	
+	@Id
+	
+	@Column(length = 15)
+	String aerolinea;
 
 }
--- Vuelo/src/com/tuempresa/vuelo/modelo/Pasajero.java
+++ Vuelo/src/com/tuempresa/vuelo/modelo/Pasajero.java
@@ -3,9 +3,12 @@ package com.tuempresa.vuelo.modelo;
 import javax.persistence.*;
 
 import lombok.*;
+
+@Entity
 @Getter @Setter
 public class Pasajero {
-@Id
-@Column(length =16)
-String nombre;
+
+	@Id
+	@Column(length =16)
+	String nombre;
 }
